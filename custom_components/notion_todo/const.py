c4dde3b05da2285936059bb15a73de3a03399401
Your Name <you@example.com>
2026-02-01 23:31:24 +0100

    test

--- custom_components/notion_todo/const.py
+++ custom_components/notion_todo/const.py
@@ -14,3 +14,4 @@ TASK_DATE_PROPERTY = "notion%3A%2F%2Ftasks%2Fdue_date_property"
 TASK_ASSIGNEE_PROPERTY = "notion%3A%2F%2Ftasks%2Fassign_property"
 TASK_STATUS_PROPERTY = "notion%3A%2F%2Ftasks%2Fstatus_property"
 TASK_DESCRIPTION_PROPERTY = "notion%3A%2F%2Ftasks%2Fai_summary_property"
+
